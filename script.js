3dc63f2e6e63a59945976438eca93a059f3e56fe
Tiago dos saantos da silva <xlzthiago@gmail.com>
2022-03-21 08:47:32 -0300

    projeto final

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+
+let areas = {
+    a: null,
+    b: null,
+    c:null
+}
 document.querySelectorAll('.item').forEach((item)=>{
     item.addEventListener('dragstart', dragStart);
     item.addEventListener('dragend', dragEnd);
@@ -8,7 +14,16 @@ document.querySelectorAll('.area').forEach((item)=>{
     item.addEventListener('dragleave', dragLeave);
     item.addEventListener('drop', drop);
 
-})
+});
+
+document.querySelector('.neutralArea').addEventListener('dragover', neutraDragOver);
+document.querySelector('.neutralArea').addEventListener('drop', neutraDrop);
+document.querySelector('.neutralArea').addEventListener('dragleave', neutraDragLeave);
+
+
+
+
+// funções do item
 function dragStart(e) {
     e.currentTarget.classList.add('dragging');
 }
@@ -38,5 +53,55 @@ function drop(e) {
     let dragItem = document.querySelector('.item.dragging');
     if (e.currentTarget.querySelector('.item') === null) {
         e.currentTarget.appendChild(dragItem);
+        updateAreas();
     }
+}
+
+
+
+///funcções da área
+
+function neutraDragLeave(e){
+    e.currentTarget.classList.remove('hover')
+
+}
+
+
+
+
+function neutraDragOver(e){
+    e.preventDefault(); 
+    e.currentTarget.classList.add('hover')
+}
+
+function neutraDrop(e){
+    e.currentTarget.classList.remove('hover');
+    let dragItem = document.querySelector('.item.dragging');
+    e.currentTarget.appendChild(dragItem);
+    updateAreas();
+
+    
+    
+}
+
+//logica das areas
+
+function updateAreas(){
+    document.querySelectorAll('.area').forEach((item)=>{
+        let name = item.getAttribute('data-name');
+
+        if(item.querySelector('.item') !== null){
+            areas[name] = item.querySelector('.item').textContent;
+            
+        }else{
+            areas[name] = null;
+        }
+
+        if (areas.a === '1' && areas.b === '2' && areas.c === '3') {
+            document.querySelector('.areas').classList.add('correct');
+        }else{
+            document.querySelector('.areas').classList.remove('correct');
+
+        }
+    })
 }
